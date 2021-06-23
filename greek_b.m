62782a3a5cb2ad7b0c76319222a226a5dc593038
JOSEALM3IDA <jfeaisec@gmail.com>
2021-06-23 15:48:26 +0100

    Começo da alinea c - ler da pasta 3 e simular

--- greek_b.m
+++ greek_b.m
@@ -49,16 +49,16 @@ net = feedforwardnet([20 20 20 20 20 20]);
 
 net.trainFcn = 'trainscg';
 net.layers{1}.transferFcn = 'tansig';
-net.layers{2}.transferFcn = 'tansig';
-net.layers{3}.transferFcn = 'tansig';
+net.layers{2}.transferFcn = 'logsig';
+net.layers{3}.transferFcn = 'purelin';
 net.layers{4}.transferFcn = 'tansig';
 net.layers{5}.transferFcn = 'tansig';
-net.layers{6}.transferFcn = 'tansig';
+net.layers{6}.transferFcn = 'logsig';
 net.layers{7}.transferFcn = 'purelin';
 net.divideFcn = 'dividerand';
-net.divideParam.trainRatio = 0.5;
+net.divideParam.trainRatio = 1;
 net.divideParam.valRatio = 0;
-net.divideParam.testRatio = 0.5;
+net.divideParam.testRatio = 0;
 
 %view(net)
 
---
+++ greek_c.m
@@ -0,0 +1,50 @@
+function greek_b()
+
+clc;
+clear all;
+close all;
+
+IMG_SCALE = 0.25;
+
+folderImg = dir('Pasta3\\letter_bnw_test_*.jpg');
+imgFiles = natsort({folderImg.name});
+
+letrasBW = zeros(3024 * 3024 * IMG_SCALE * IMG_SCALE, length(imgFiles));
+letrasTarget = [];
+letrasBWCol = 1;
+for i=1:length(imgFiles)/10   
+    for j=1:10
+        img = imread(sprintf('Pasta3\\%s', char(imgFiles(((j - 1) * 10) + i))));
+        img = imresize(img, IMG_SCALE);
+        binarizedImg = imbinarize(img);
+        letrasBW(:, letrasBWCol) = reshape(binarizedImg, 1, []);
+        letrasBWCol = letrasBWCol + 1;
+    end
+    
+    letrasTarget = [letrasTarget eye(10)];
+end
+
+
+
+% CARREGAR NET
+net = load('net46');
+view(net)
+
+out = sim(net, letrasBW)
+r = 0;
+for i=1:size(out,2)               % Para cada classificacao  
+    [a b] = max(out(:,i));        %b guarda a linha onde encontrou valor mais alto da saida obtida
+    [c d] = max(letrasTarget(:,i)); %d guarda a linha onde encontrou valor mais alto da saida desejada
+    if b == d                       % se estao na mesma linha, a classificacao foi correta (incrementa 1)
+      r = r+1;
+    end
+end
+
+plotconfusion(letrasTarget, out)
+%plotperf(tr)
+
+accuracy = r/size(out,2);
+fprintf('Precisao total %f\n', accuracy)
+
+end
+
